deb95e312fd564acd3c94a7ded870c8a4e426471
doraneko94 <shuntaro94@gmail.com>
2024-11-01 08:52:27 +0900

    fix webp

--- to_webp.py
+++ to_webp.py
@@ -8,5 +8,5 @@ if len(args) < 2:
     raise ValueError
 filepath = args[1]
 
-img = cv2.imread(filepath)
+img = Image.fromarray(cv2.cvtColor(cv2.imread(filepath), cv2.COLOR_BGR2RGB))
 img.save(filepath.split(".")[0] + ".webp")
